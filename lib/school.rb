156bebc6d1effe48481dfaf773d7b0b006b018d7
CBreakr <github email address>
2020-02-20 18:10:38 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+    attr_reader :name
+    attr_reader :roster
+
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if !@roster.has_key?(grade) then
+            @roster[grade] = []
+        end
+        @roster[grade] << student
+    end
+
+    def grade(student_grade)
+        @roster[student_grade]
+    end
+
+    def sort
+        @roster.map do |key, value|
+            [key, value.sort]
+        end.to_h
+    end
+end
