7130924a7de956567aada1b6649fc392a9b313d9
JohnsonNYC <jkow95@gmail.com>
2020-02-20 22:03:36 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+
+
+class School
+    attr_accessor :name, :roster
+  
+    def initialize(name) #starts when every instance is made
+      @name = name       
+      @roster = {}       #hash created for every instance
+    end
+  
+    def add_student(student_name, grade) 
+      roster[grade] ||= []  #if the grade is undefined, evaluate [] and set the grade as the default
+      roster[grade] << student_name 
+    end
+    
+    def grade(number)
+        roster[number]
+    end
+
+    def sort 
+        sorted = {}
+        roster.each do |grade,student_object|
+            sorted[grade] = student_object.sort
+        end
+        sorted
+    end 
+end
