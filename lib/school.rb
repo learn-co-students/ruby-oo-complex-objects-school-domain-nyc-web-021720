1327cbf22870b360402a4957fb25f375532ac46d
github username <github email address>
2020-02-20 22:57:44 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School 
+
+    attr_reader :name, :roster 
+ 
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end 
+
+    def add_student(student_name, grade)
+        roster[grade] ||= []
+        roster[grade] << student_name
+    end
+
+    def grade(students_in_grade)
+        roster[students_in_grade]
+    end 
+
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+            sorted[grade] = students.sort 
+    end 
+    sorted
+    end
+end 
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
