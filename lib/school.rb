3766715ada6d16123ac388532d4feb5702b1d5f6
matthewpeak <matthewpea@gmail.com>
2020-02-24 14:29:12 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+    attr_accessor :school_name, :roster 
+    
+    def initialize(school_name)
+        @school_name= school_name 
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        roster[grade] ||= []
+        roster[grade].push(student_name) 
+    end
+
+    def grade(grade) 
+        @roster[grade]
+    end  
+
+    def sort 
+     @roster.each_value{|value| value.sort!}
+    end 
+        
+end
+
