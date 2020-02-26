a2ecaac7b2190f6d3cc13d86d46d09460a53b782
github username <jeffmussels@gmail.com>
2020-02-26 11:43:03 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name=name 
+        @roster = Hash.new{|k,v| k[v]=[]}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.transform_values{|v| v.sort}
+    end
+
+
+
+end
